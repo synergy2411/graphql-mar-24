e36976dc7031f9459637858df0a000416184802c
Sumit Khandelwal <synergy2411@outlook.com>
2024-03-12 14:32:11 +0530

    yoga server started

--- 02-graphql-prisma/src/main.js
+++ 02-graphql-prisma/src/main.js
@@ -1,8 +1,38 @@
+import { createServer } from "node:http";
+import { createSchema, createYoga } from "graphql-yoga";
+
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-async function main() {}
+const typeDefs = `
+type Query {
+    hello : String!
+}
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => "Hello Prisma",
+  },
+};
+
+const startServer = () => {
+  const schema = createSchema({
+    typeDefs,
+    resolvers,
+  });
+
+  const yoga = createYoga({ schema });
+
+  const server = createServer(yoga);
+
+  server.listen(4044, () => console.log("Yoga Server started at PORT : 4044"));
+};
+
+async function main() {
+  startServer();
+}
 
 main()
   .catch((err) => console.error(err))
