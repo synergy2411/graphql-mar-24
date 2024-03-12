b278e10c41627ecdda219c1e4a5a1fe6c2026185
Sumit Khandelwal <synergy2411@outlook.com>
2024-03-12 13:29:04 +0530

    packages install

--- 02-graphql-prisma/src/main.js
+++ 02-graphql-prisma/src/main.js
@@ -2,17 +2,7 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-async function main() {
-  const createdUser = await prisma.user.create({
-    data: {
-      name: "ross",
-      email: "ross@test.com",
-      age: 25,
-    },
-  });
-
-  console.log(createdUser);
-}
+async function main() {}
 
 main()
   .catch((err) => console.error(err))
