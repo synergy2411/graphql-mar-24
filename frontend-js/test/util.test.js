881ee5f43c37dee14db9184903f994f86f11bc36
Sumit Khandelwal <synergy2411@outlook.com>
2024-03-13 11:41:04 +0530

    jest onfig added

--- frontend-js/test/util.test.js
+++ frontend-js/test/util.test.js
@@ -2,10 +2,22 @@
 // import { getFirstName} from "../utils/validator"
 
 // CommonJS import
-const { getFirstName } = require("../utils/validator");
+const { getFirstName, lengthValidator } = require("../utils/validator");
 
 test("Should returns the first name", () => {
   const firstName = getFirstName("Rachel Green");
 
   expect(firstName).toBe("Rachel");
+});
+
+test("Should return true if password length is greater then 8 characters", () => {
+  const isGreaterThan8 = lengthValidator("pwd12345");
+
+  expect(isGreaterThan8).toBeTruthy();
+});
+
+test("Should return false if password length is less than 8 characters", () => {
+  const isLessThan8 = lengthValidator("pwd123");
+
+  expect(isLessThan8).toBeFalsy();
 });
